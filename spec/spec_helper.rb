5460ad29e572af34c881df7a398e56b15079e131
Takeshi Kishi <takeya0x86@gmail.com>
2020-04-21 23:39:31 +0900

    enable headless only on github actions

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -111,18 +111,20 @@ RSpec.configure do |config|
 end
 
 # Capybara settings
-Capybara.register_driver :selenium_chrome_headless do |app|
+Capybara.register_driver :selenium_chrome do |app|
   Capybara::Selenium::Driver.load_selenium
   options = Selenium::WebDriver::Chrome::Options.new
-  options.add_argument('headless')
-  options.add_argument('disable-gpu') if Gem.win_platform?
+  if ENV['GITHUB_ACTIONS'] == 'true'
+    options.add_argument('headless')
+    options.add_argument('disable-gpu') if Gem.win_platform?
+  end
   # Workaround https://bugs.chromium.org/p/chromedriver/issues/detail?id=2650&q=load&sort=-id&colspec=ID%20Status%20Pri%20Owner%20Summary
   options.add_argument('disable-site-isolation-trials')
   Capybara::Selenium::Driver.new(app, browser: :chrome, clear_local_storage: false, options: options)
 end
 
 Capybara.configure do |config|
-  config.default_driver = :selenium_chrome_headless
+  config.default_driver = :selenium_chrome
   config.app_host = 'https://hotel.testplanisphere.dev'
 end
 
